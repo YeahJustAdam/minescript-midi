79b3f03156beff325730c35709a9263bed3bb342
Adam Kijonka <adamkijonka.work@gmail.com>
2025-09-21 13:42:03 +0200

    retructure

--- scripts/test.py
+++
@@ -1,9 +0,0 @@
-import mido 
-import minescript as ms
-import time
-
-carts = ms.get_entities()
-entities = ms.entities()
-for e in entities:
-    strings = f"{e.name}, {e.type}"
-    ms.chat(strings)
